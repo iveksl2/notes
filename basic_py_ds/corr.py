bade5eeacf73e890186c077f552f899936591456
iveksl2 <iveksl2@gmail.com>
2017-12-25 22:36:35 -0600

    Add additional notes about vectorization/broadcasting/tips from coursera DL class

--- basic_py_ds/corr.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-path = 'http://archive.ics.uci.edu/ml/machine-learning-databases/auto-mpg/auto-mpg.data'
-
-mpg_data = pd.read_csv(path, delim_whitespace=True, header=None,
-                    names = ['mpg', 'cylinders', 'displacement','horsepower',
-                                    'weight', 'acceleration', 'model_year', 'origin', 'name'],
-                                na_values='?')
